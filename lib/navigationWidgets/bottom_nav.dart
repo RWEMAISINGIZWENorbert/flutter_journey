dcdaa05e8925681da63b4f90a1f94f0f142f7f19
rwema <rwema916@gmail.com>
2025-02-07 15:35:46 +0200

    Bottom navigation Bar 1

--- lib/navigationWidgets/bottom_nav.dart
+++ lib/navigationWidgets/bottom_nav.dart
@@ -1,17 +1,40 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_journey/main.dart';
 
 class BottomNav extends StatelessWidget {
   const BottomNav({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-       bottomNavigationBar: NavigationDestination(
-            icon: Icon(Icons.home),
-            label: "Home",
-       )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+         bottomNavigationBar: NavigationBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          onDestinationSelected: (index)=> {},
+          destinations: const[
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.analytics), label: 'Analytics'),
+            NavigationDestination(icon: Icon(Icons.report_sharp), label: 'Reports'),
+            NavigationDestination(icon: Icon(Icons.move_down), label: 'Invices'),
+          ],
+          ),
+          body: Container(),
+      ),
+
     );
   }
 }
+
+// class NavigationController extends GetxController {
+//   final Rx<int> = 0.abs;
+ 
+//  final screen = [
+//     Container(color: Colors.blue,),
+//     Container(color: Colors.amber,),
+//     Container(color: Colors.green,),
+//     Container(color: Colors.black26,)
+//  ];
+
+// }
